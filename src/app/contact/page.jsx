4027759f4dfb4b215a2c514918005b9c7dfa44f8
gustav <rhezagustam@gmail.com>
2023-12-23 16:07:00 +0700

    second commit

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div className='pt-20 min-h-[50vh] flex'>
+      <div className='m-auto flex flex-col gap-y-2'>
+        <h2 className='text-center text-3xl font-semibold'>Contact Page</h2>
+        <Link
+          className='text-center bg-orange-500 text-white hover:bg-orange-600 px-2 py-1 w-fit rounded-lg mx-auto block'
+          href='/ideas'
+        >
+          Go to ideas page
+        </Link>
+      </div>
+    </div>
+  );
+}
